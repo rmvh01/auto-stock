82e8a6ffa27dc9caf28b6e77bd760018b5b30d1a
farellfebriano <farellfebriano8@gmail.com>
2023-07-24 17:47:23 -0500

    creating a models and register it

--- sales/api/sales_rest/admin.py
+++ sales/api/sales_rest/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from models import AutomobileVO, SalesPerson, Customer, Sale
 
-# Register your models here.
+
+@admin.register(AutomobileVO)
+class AutomobileVOAdmin(admin.ModelAdmin):
+    list_display = ['vin','sold']
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name',"last_name","id"]
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ['SalesPerson','customer','id']
+
+@admin.register(SalesPerson)
+class SalesPerson(admin.ModelAdmin):
+     list_display = ["first_name",'id']
--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=100)
+    sold = models.BooleanField(default=False)
 
-# Create your models here.
+class SalesPerson(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    employee_id = models.CharField(max_length=150)
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    phone_number = models.IntegerField(max_length=100)
+
+class Sale(models.Model):
+    automobile = models.ForeignKey(
+        AutomobileVO, related_name="automobile",
+        on_delete=models.PROTECT
+    )
+    SalesPerson = models.ForeignKey(
+        SalesPerson, related_name='SalesPerson',
+        on_delete=models.PROTECT
+    )
+    customer = models.ForeignKey(
+        Customer, related_name='customer',
+        on_delete=models.PROTECT,
+    )
+    price = models.IntegerField(max_length=100)
